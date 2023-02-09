52720bf13de330c3bf27a73fef07080888eb629d
Faris Alsalih <falsalih@aol.de>
2023-02-09 21:55:56 +0100

    del 'history' code, del 'host', add comments

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from "./rootReducer";
 import { Helmet } from 'react-helmet';
-import {createBrowserHistory} from 'history';
+// import {createBrowserHistory} from 'history';
 import { Provider } from 'react-redux'
 
+// the Redux store related code should be in seperate file (e.g. store.js) inside a properly structured redux folder
+// FAL: consider switching to Redux ToolKit and replacing the deprecated createStore with configureStore from the TK
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk)
 ));
+// add redux-logger to store
 
 
-
-export const history = createBrowserHistory();
+// export const history = createBrowserHistory();
 
 if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
     var msViewportStyle = document.createElement('style');
@@ -28,7 +30,7 @@ if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
     document.head.appendChild(msViewportStyle);
 }
 
-let host = window.location.protocol + "//" +window.location.hostname;
+// let host = window.location.protocol + "//" +window.location.hostname;
 
 ReactDOM.render((
     <Provider store={store}>
