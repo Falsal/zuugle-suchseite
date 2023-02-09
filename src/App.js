eb5dbe81eb5d384b558b5967ae0ce20af9e78e81
Faris Alsalih <falsalih@aol.de>
2023-02-09 21:53:57 +0100

    del 'history' , testComp, comment out Matomo

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import Start from "./views/Start/Start";
 //import Main from "./views/Main/Main";
 import ModalRoot from "./components/ModalRoot";
 import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+// import {createBrowserHistory} from 'history';
 import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
 import {lazy, Suspense} from "react";
 import CircularProgress from "@mui/material/CircularProgress";
+// import TestComp from '../TestComp';
 const Main = lazy(() => import('./views/Main/Main'));
 const About = lazy(() => import('./views/Pages/About'));
 const Impressum = lazy(() => import('./views/Pages/Impressum'));
 const Privacy = lazy(() => import('./views/Pages/Privacy'));
 
 function App() {
+    // const history = createBrowserHistory();
+
     let siteId = 11;
     if (process.env.NODE_ENV !== "production") {
       siteId = 9;
     }
-
+    // ******  MATOMO CODE ******
     // const instance = createInstance({
     //     urlBase: `${window.location.protocol}//${window.location.host}`,
     //     siteId: 9,
@@ -40,12 +44,13 @@ function App() {
     // })
 
 
+    // <MatomoProvider value={instance}>
     return (
-        // <MatomoProvider value={instance}>
             <ThemeProvider theme={theme}>
                 <div className="App">
                     <Suspense fallback={<div style={{height: "100%", width: "100%", padding: "20px"}}><CircularProgress /></div>}>
-                        <BrowserRouter history={history}>
+                        {/* <BrowserRouter history={history}> */}
+                        <BrowserRouter >
                             <Routes>
                                 <Route path="/" element={<Start/>}/>
                                 <Route path="/suche" element={<Main/>}/>
@@ -53,6 +58,7 @@ function App() {
                                 <Route path="/imprint" element={<Impressum/>}/>
                                 <Route path="/privacy" element={<Privacy/>}/>
                                 <Route path="/:city" element={<Main/>}/>
+                                {/* <Route path="/cache" element={<TestComp/>}/> */}
 
                                 <Route
                                     path="*"
@@ -64,8 +70,8 @@ function App() {
                 </div>
                 <ModalRoot />
             </ThemeProvider>
-        // </MatomoProvider>
     );
+    // </MatomoProvider>
 }
 
 export default App;
