b0ae63924f9a23af29671543812bd4f8f72aef71
Faris Alsalih <falsalih@aol.de>
2023-02-09 21:50:28 +0100

    add proxy port 8080 for development

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ app.use(compression());
 app.listen(port, () => {console.log("Listening on port " + port)});
 
 app.use("/app_static", express.static(path.join(__dirname, '../suche/app_static')));
-app.use('/api', proxy({target: 'http://localhost:6060', secure: false}));
+if(process.env.NODE_ENV === "production"){
+  proxy_port = 6060;
+}else {
+  proxy_port = 8080;
+
+}
+app.use('/api', proxy({target: `http://localhost:${proxy_port}`, secure: false}));
 app.use("/public", express.static(path.join(__dirname, '../api/public'), {fallthrough: false}));
 console.log(path.join(__dirname, '../api/public'))
 app.use((req, res, next) => {
