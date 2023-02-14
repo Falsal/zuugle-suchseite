494c07f5890f97478b9b3466ccc8a82bcd2fbe12
Faris Alsalih <falsalih@aol.de>
2023-02-14 20:53:48 +0100

    comment redundant code

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(compression());
 
 app.listen(port, () => {console.log("Listening on port " + port)});
 
-app.use("/app_static", express.static(path.join(__dirname, '../suche/app_static')));
+// app.use("/app_static", express.static(path.join(__dirname, '../suche/app_static'))); // redundant legacy code
 if(process.env.NODE_ENV === "production"){
   proxy_port = 6060;
 }else {
@@ -19,13 +19,18 @@ if(process.env.NODE_ENV === "production"){
 }
 app.use('/api', proxy({target: `http://localhost:${proxy_port}`, secure: false}));
 app.use("/public", express.static(path.join(__dirname, '../api/public'), {fallthrough: false}));
-console.log(path.join(__dirname, '../api/public'))
-app.use((req, res, next) => {
-  //res.append('Access-Control-Allow-Origin', ['*']);
-  //res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// console.log:
+// console.log('path to public folder from server :',path.join(__dirname, '../api/public'))
+// console.log("target " , `http://localhost:${proxy_port}`)
+
+// redundant legacy code
+// since we are doing the corsConfig on the backend server "zuugle-api" so we do not need to set cors headers here
+// app.use((req, res, next) => {
+//   res.append('Access-Control-Allow-Origin', ['*']);
+//   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.append('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.get([ '/*' ], function(req, res) {
   res.sendFile(path.join(__dirname, '../suche/index.html'));
